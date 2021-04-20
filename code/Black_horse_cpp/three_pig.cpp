7f298f8334dab112f54e02cd39f614d1f91cf343
Could you give a cat? <43361054+LingfengPang@users.noreply.github.com>
2021-04-20 22:31:07 +0800

    [update] c++note & leetcode

---
+++ code/Black_horse_cpp/three_pig.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    char ch = 'A';
+    int max = a;
+    cout << "Please Input 3 pigs' age"<<endl;
+    cin >> a >> b >> c;
+    cout << "A is "<<a<<endl;
+    cout << "B is "<<b<<endl;
+    cout << "C is "<<c<<endl;
+    if(max < b){
+        max = b;
+        ch = 'B';
+    }
+
+    if(max < c){
+        max = c;
+        ch = 'C';
+    }
+
+    cout << ch<<" is old! it is "<<max<<endl;
+    system("pause");
+}
